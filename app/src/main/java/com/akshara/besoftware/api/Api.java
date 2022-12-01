00452b2016a30f12640321d43913c4c40724199b
Shubham Agrawal <shubham.agrawal20310@gmail.com>
2022-12-01 11:11:26 +0545

    Implementation of Retrofit and Api's

--- app/src/main/java/com/akshara/besoftware/api/Api.java
+++ app/src/main/java/com/akshara/besoftware/api/Api.java
@@ -1,5 +1,5 @@
 package com.akshara.besoftware.api;
 
 public class Api {
-    public static final String BASE_URL = "";
+    public static final String BASE_URL = "https://oishirestaurant.akshara.com.np/";
 }
